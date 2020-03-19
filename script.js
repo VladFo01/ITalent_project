7c9875792f9874a81d062b2bf4b27b0fb3aaf086
VladFo01 <62206631+VladFo01@users.noreply.github.com>
2020-03-19 18:43:48 +0200

    Add index.html script.js style.css
    Это вторая домашняя робота вместе с усложненным заданием

--- script.js
+++ script.js
@@ -6,9 +6,38 @@ const formSearch=document.querySelector('.form-search'),
     inputCitiesTo=document.querySelector('.input__cities-to'),
     dropdownCitiesTo=document.querySelector('.dropdown__cities-to');
 
+//данные API
+const citiesApi = 'dataBase/cities.json',
+    proxy = 'https://cors-anywhere.herokuapp.com/',
+    apiKey = '319d551dcd90c268f0f983cf1f6507f8',
+    calendar='http://min-prices.aviasales.ru/calendar_preload';
+
+//25 мая - Екатеринбург - Калининград
+//Вывести в консоль
+
+
 //массив подставляемых данных(города)
-const city=['Москва','Санкт-Петербург','Минск','Караганда','Челябинск','Керч','Волгоград','Самара',
-    'Днепр','Екатеригбург','Киев'];
+let city=[];
+
+//функции
+
+const getData=(url, callback)=>{
+    const request=new XMLHttpRequest();
+
+    request.open('GET',url);
+
+    request.addEventListener('readystatechange', ()=>{
+        if(request.readyState!==4) return;
+
+        if(request.status===200){
+            callback(request.response);
+        } else{
+            console.error(request.status);
+        }
+    });
+
+    request.send();
+};
 
 
 //функция для отбражения списка городов
@@ -16,19 +45,16 @@ const showCity=(input, list)=>{
     list.textContent='';
 
     if(input.value!==''){
-    
-
         const filterCity = city.filter((item)=>{
-            const fixItem = item.toLowerCase();
-            return fixItem.includes(input.value.toLowerCase());
+                const fixItem = item.name.toLowerCase();
+                return fixItem.includes(input.value.toLowerCase());
         });
 
         filterCity.forEach((item)=>{
             const li = document.createElement('li');
             li.classList.add('dropdown__city');
-            li.textContent=item;
+            li.textContent=item.name+':'+item.code;
             list.append(li);
-            console.log(li);
         });
     }
 }
@@ -41,7 +67,7 @@ const list=(input, dropdown)=>{
         showCity(input, dropdown);
     });
 
-    //отслеживание клика на параметры всплывающего списка
+    //отслеживание клика на параметры всплывающего списка и подстановка текста в поле
     dropdown.addEventListener('click',(event)=>{
         const target=event.target;
         if(target.tagName.toLowerCase()==='li'){
@@ -52,5 +78,22 @@ const list=(input, dropdown)=>{
     
 }
 
+//вызовы функций
+
 list(inputCitiesFrom, dropdownCitiesFrom);
 list(inputCitiesTo, dropdownCitiesTo);
+
+getData(citiesApi, (data)=>{
+    city=JSON.parse(data).filter(item=>item.name)
+});
+
+getData(calendar+'?&origin=SVX&destination=KGD&depart_date=2020-05-25', (data)=>{
+    console.log(JSON.parse(data));
+});
+/*
+
+(item)=>{
+    return item.name
+}
+
+*/
